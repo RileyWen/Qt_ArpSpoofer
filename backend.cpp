531a3bc81e9aa682ccb2ff6c94f092fa02755909
Riley Wen <rileywen.work@gmail.com>
2019-09-13 14:12:21 +0800

    Initial commit

---
+++ backend.cpp
@@ -0,0 +1,19 @@
+#include "backend.h"
+
+BackEnd::BackEnd(QObject *parent) :
+        QObject(parent) {
+}
+
+QString BackEnd::userName() {
+    return m_userName;
+}
+
+void BackEnd::setUserName(const QString &userName) {
+    if (userName == m_userName)
+        return;
+
+    m_userName = userName;
+
+    qDebug() << "userName Changed to: " << m_userName;
+    emit userNameChanged();
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+
+#include "backend.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    qmlRegisterType<BackEnd>("io.qt.examples.backend", 1, 0, "BackEnd");
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
+}
+
